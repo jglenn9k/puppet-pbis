9a3c3db0736404599f95f0a19969aeb1809840da
James Glenn <thedonkdonk@gmail.com>
2015-04-01 13:17:36 -0500

    Fix regex

--- manifests/params.pp
+++ manifests/params.pp
@@ -32,12 +32,12 @@ class pbis::params {
   # automatically.
   $package_file = $::osfamily ? {
     'Debian'          => "${package}.${::architecture}.deb",
-    '/(RedHat|Suse)/' => "${package}.${::architecture}.rpm",
+    /(RedHat|Suse)/ => "${package}.${::architecture}.rpm",
     default           => fail("Unsupported operating system: ${::operatingsystem}."),
   }
   $package_file_provider = $::osfamily ? {
     'Debian'          => 'dpkg',
-    '/(RedHat|Suse)/' => 'rpm',
+    /(RedHat|Suse)/ => 'rpm',
     default           => fail("Unsupported operating system: ${::operatingsystem}."),
   }
 }
